2bde486c4636703944a2073e8951d5a7623b169a
dds <damandeep.in@gmail.com>
2021-05-31 12:19:34 +0530

    changed

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 // Get the modal
-window.onload = function () {
+
+
 var modal = document.getElementById("myModal");
+
+
+
+
+window.onload = function(){
+  var m=document.getElementById("myModal");
+    m.style.display="block"; 
+}
+
+
+
+
+
 
 // button
 var btn = document.getElementById("myBtn");
 
 
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+if(btn){
+btn.onclick()=()=>{
+  modal.style.display="block";
+}
 }
 
 
+// When the user clicks on the button, open the modal
 
+
+
+/*
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
-}
+
+*/
